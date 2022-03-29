3a68a16369b8dcf0fdd15ce9ce447af3f81f857e
Bang Vo <bavo.imp@gmail.com>
2022-03-29 08:30:32 +0700

    mask rcnn config

--- configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py
+++ configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py
@@ -3,3 +3,13 @@ _base_ = [
     '../_base_/datasets/coco_instance.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
+
+runner = dict(_delete_=True, type='IterBasedRunner', max_iters=1000)
+
+checkpoint_config = dict(interval=100)
+
+data = dict(
+    samples_per_gpu=3)
+
+optimizer_config = dict(
+ _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
