8d93c7d24a69aae852c4328f7b215bb17a8ab0c4
bavo96 <bavo.imp@gmail.com>
2022-04-04 22:11:37 +0700

    get subset data

--- data/subset_data.py
+++ data/subset_data.py
@@ -1,40 +1,50 @@
 import json
+from re import S
 from tqdm import tqdm
 import shutil
 from pathlib import Path
 
-final_path = 'coco2017_subset/'
-src_parent = 'train2017/'
-dst_parent = final_path + 'train2017/'
-anno_parent = final_path + 'annotations/'
+# Download full coco2017 (images+annotations) train+val first
 
-shutil.rmtree(final_path, ignore_errors=True)
-Path(dst_parent).mkdir(parents=True, exist_ok=True)
-Path(anno_parent).mkdir(parents=True, exist_ok=True)
+def get_subset(mode):
+  src_img_parent = '{}2017/'.format(mode)
+  src_anno_parent = 'annotations/'
 
-# 'person': 1, 'car': 3, 'cat': 17, 'bottle': 44, 'chair': 62
-id_cls = [3, 17, 44, 62]
+  final_path = 'coco2017{}_subset/'.format(mode)
+  dst_img_parent = final_path + 'images/'
+  dst_anno_parent = final_path + 'annotations/'
 
-# Download full coco2017 (images+annotations) first
-with open("annotations/instances_train2017.json", "r") as f:
-  data = json.load(f)
+  shutil.rmtree(final_path, ignore_errors=True)
+  Path(dst_img_parent).mkdir(parents=True, exist_ok=True)
+  Path(dst_anno_parent).mkdir(parents=True, exist_ok=True)
 
-  annotations = []
-  images = []
-  img_id = []
+  # 'person': 1, 'car': 3, 'cat': 17, 'bottle': 44, 'chair': 62
+  id_cls = [3, 17]
 
-  dict_images = {img['id']:img for img in data['images']}
+ 
+  with open("{}/instances_{}2017.json".format(src_anno_parent, mode), "r") as f:
+    data = json.load(f)
 
-  for anno in tqdm(data['annotations']):
-    if anno['category_id'] in id_cls:
-      annotations.append(anno)
-      
-      if anno['image_id'] not in img_id:
-        img_id.append(anno['image_id'])
-        images.append(dict_images[anno['image_id']])
-        shutil.copyfile(src_parent + dict_images[anno['image_id']]['file_name'], dst_parent + dict_images[anno['image_id']]['file_name'])
-  data['images'] = images
-  data['annotations'] = annotations
+    annotations = []
+    images = []
+    img_id = []
 
-  with open(anno_parent + 'instances_train2017.json', 'w') as f:
-    json.dump(data, f)
+    dict_images = {img['id']:img for img in data['images']}
+
+    for anno in tqdm(data['annotations']):
+      if anno['category_id'] in id_cls:
+        annotations.append(anno)
+        
+        if anno['image_id'] not in img_id:
+          img_id.append(anno['image_id'])
+          images.append(dict_images[anno['image_id']])
+          shutil.copyfile(src_img_parent + dict_images[anno['image_id']]['file_name'], dst_img_parent + dict_images[anno['image_id']]['file_name'])
+    data['images'] = images
+    data['annotations'] = annotations
+
+    with open(dst_anno_parent + 'instances_{}2017.json'.format(mode), 'w') as f:
+      json.dump(data, f)
+
+if __name__ == "__main__":
+  get_subset('train')
+  get_subset('val')
