8a7c8819a93ed26ca70c3a059a6d68cef4a25c02
bavo96 <bavo.imp@gmail.com>
2022-04-05 23:31:05 +0700

    tensorboard interval

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -4,7 +4,7 @@ log_config = dict(
     interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
-        dict(type='TensorboardLoggerHook')
+        dict(type='TensorboardLoggerHook', interval=1)
     ])
 # yapf:enable
 custom_hooks = [dict(type='NumClassCheckHook')]
