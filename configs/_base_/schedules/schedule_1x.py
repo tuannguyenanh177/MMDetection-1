2ed4b0353c54d4a13f9d8da48269bf7dbe7d8e57
bavo96 <bavo.imp@gmail.com>
2022-04-06 00:40:35 +0700

    adjust learning rate

--- configs/_base_/schedules/schedule_1x.py
+++ configs/_base_/schedules/schedule_1x.py
@@ -1,11 +1,11 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
-    warmup_iters=500,
+    warmup_iters=100,
     warmup_ratio=0.001,
-    step=[8, 11])
+    step=[600, 900])
 runner = dict(type='EpochBasedRunner', max_epochs=12)
