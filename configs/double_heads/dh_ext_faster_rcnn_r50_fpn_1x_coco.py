5c9bff4463dbc3c8cf1e299ce1de548258b52dde
Bang Vo <bavo.imp@gmail.com>
2022-04-06 12:32:29 +0700

    Update dh_ext_faster_rcnn_r50_fpn_1x_coco.py

--- configs/double_heads/dh_ext_faster_rcnn_r50_fpn_1x_coco.py
+++ configs/double_heads/dh_ext_faster_rcnn_r50_fpn_1x_coco.py
@@ -30,7 +30,7 @@ model = dict(
                 type='CrossEntropyLoss', use_sigmoid=False, loss_weight=2.0),
             loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=2.5))))
 
-runner = dict(_delete_=True, type='IterBasedRunner', max_iters=1000)
+runner = dict(_delete_=True, type='IterBasedRunner', max_iters=10000)
 
 checkpoint_config = dict(interval=100)
 
