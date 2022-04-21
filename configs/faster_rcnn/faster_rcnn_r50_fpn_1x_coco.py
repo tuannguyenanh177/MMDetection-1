88d141cf594e816748bc929bf4d9bbf4d9cce3d7
Bang Vo <bavo.imp@gmail.com>
2022-04-21 23:40:14 +0700

    config faster_rcnn

--- configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py
@@ -3,3 +3,17 @@ _base_ = [
     '../_base_/datasets/coco_detection.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
+
+runner = dict(_delete_=True, type='IterBasedRunner', max_iters=10000)
+
+checkpoint_config = dict(interval=20000)
+
+data = dict(
+    samples_per_gpu=3)
+
+optimizer_config = dict(
+ _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+
+log_config = dict(interval=1) # print log every inter (included loss value)
+
+evaluation = dict(interval=1000, metric='bbox', save_best='bbox_mAP') # evaluate every 100 iters
